f940835d9f12c69c5f680de9fd31a8f570b56854
Daisia Frank <142124530+daisiafrank@users.noreply.github.com>
2023-09-20 08:03:30 -0700

    Update Netflix Title Data.sql

--- Netflix Title Data.sql
+++ Netflix Title Data.sql
@@ -1,3 +1,33 @@
 SELECT title, date_added
 FROM netflix
 WHERE date_added > '2022-01-01';
+SELECT [title]
+FROM [netflix]
+WHERE [type] = 'Movie' AND [release_year] < 2014;
+SELECT type, COUNT(*) as count
+FROM netflix
+GROUP BY type;
+SELECT country, COUNT(*) as count
+FROM netflix
+GROUP BY country
+ORDER BY count DESC
+LIMIT 5;
+SELECT title
+FROM netflix
+WHERE type = 'Movie' AND rating = 'TV-MA';
+SELECT release_year, COUNT(*) as count
+FROM netflix
+GROUP BY release_year
+ORDER BY release_year DESC;
+SELECT title
+FROM netflix
+WHERE title LIKE '%love%';
+SELECT title, duration
+FROM netflix
+WHERE type = 'Movie'
+ORDER BY CAST(REPLACE(duration, ' min', '') AS INTEGER) DESC
+LIMIT 5;
+SELECT listed_in, COUNT(*) as count
+FROM netflix
+GROUP BY listed_in
+ORDER BY count DESC;
