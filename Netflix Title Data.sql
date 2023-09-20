fd576a842805d7ddf6c09dc70187e0a3f85a80d9
Daisia Frank <142124530+daisiafrank@users.noreply.github.com>
2023-09-20 08:08:14 -0700

    Update Netflix Title Data.sql

--- Netflix Title Data.sql
+++ Netflix Title Data.sql
@@ -1,3 +1,5 @@
+
+--Content Added in the Last Year:--
 SELECT title, date_added
 FROM netflix
 WHERE date_added > '2022-01-01';
@@ -5,29 +7,43 @@ WHERE date_added > '2022-01-01';
 SELECT [title]
 FROM [netflix]
 WHERE [type] = 'Movie' AND [release_year] < 2014;
+
+--Count of Movies vs. TV Shows:--
 SELECT type, COUNT(*) as count
 FROM netflix
 GROUP BY type;
+
+--Top 5 Countries Producing the Most Content:--
 SELECT country, COUNT(*) as count
 FROM netflix
 GROUP BY country
 ORDER BY count DESC
 LIMIT 5;
+
+--List of Movies with a Specific Rating (e.g., TV-MA):--
 SELECT title
 FROM netflix
 WHERE type = 'Movie' AND rating = 'TV-MA';
+
+--Number of Releases Per Year:--
 SELECT release_year, COUNT(*) as count
 FROM netflix
 GROUP BY release_year
 ORDER BY release_year DESC;
+
+--Find Titles Containing a Specific Word (e.g., "love"):--
 SELECT title
 FROM netflix
 WHERE title LIKE '%love%';
+
+--Find the Longest Movies (by duration):--
 SELECT title, duration
 FROM netflix
 WHERE type = 'Movie'
 ORDER BY CAST(REPLACE(duration, ' min', '') AS INTEGER) DESC
 LIMIT 5;
+
+--Count of Content by Genre:--
 SELECT listed_in, COUNT(*) as count
 FROM netflix
 GROUP BY listed_in
